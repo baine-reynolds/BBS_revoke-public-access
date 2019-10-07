ff44b2a4810b6bd9837219d3d855a56539466650
michaelchwalker <miwalker@atlassian.com>
2019-10-07 16:35:41 -0500

    changed so that only affected projects will be changed rather than the entire environment. Parse occurs first, based on that list, changes are made.

--- public-access-config.py
+++ public-access-config.py
@@ -11,20 +11,12 @@ class Vars():
 	headers={'X-Atlassian-Token': 'no-check', "Content-type": "application/json"}
 	api_session=None
 
-def get_projects(Vars, paged_start=None, paged_limit=None):
-	while True:
-		params = {'start': paged_start, 'limit': paged_limit}
-		endpoint_url = Vars.source_url + '/rest/api/1.0/projects'
-		r = ServerConnections.get_api(Vars, endpoint_url, params)
-		r_data = r.json()
-		for project_json in r_data['values']:
-			Vars.project_name = project_json['name']
-			Vars.project_key = project_json['key']
-			Vars.project_public = project_json['public']
-			yield Vars
-		if r_data['isLastPage'] == True:
-			return
-		paged_start = r_data['nextPageStart']
+def test_project(input_line, Vars, paged_start=None, paged_limit=None):
+	params = {'start': paged_start, 'limit': paged_limit}
+	endpoint_url = Vars.source_url + '/rest/api/1.0/projects/' + input_line[0]
+	r = ServerConnections.get_api(Vars, endpoint_url, params)
+	r_data = r.json()
+	return r_data['public'] # True if public, False if private
 
 def get_repos(Vars, paged_start=None, paged_limit=None):
 	while True:
@@ -57,26 +49,18 @@ class ServerConnections:
 		return r
 
 def parse_input():
-	#Needs to read in the file with the project/repo data of what needs to be revoked. See revoke_public_access() for example
-	#pass
-	#Example based on "example.in" input file
-	with open("example.in","r") as infile:
+	#Example based on "input.in" input file
+	with open("input.in","r") as infile:
 		contents_list = infile.readlines()
 		parsed_list = []
-		for repo in contents_list:
+		for line in contents_list:
 			# removes any whitespace or linebreaks
-			cleaned = repo.strip()
-			# converts https://subdomain.domain.com/projects/project-key/repos/repo-key to 
-			# ["https:","","subdomain","domain","com","projects","project-key","repos","repo-slug", ...]
-			split = cleaned.split('/') 
-			# finds "projects" and adds 1 to list position, grabbing the project key
-			project_index = split.index('projects')
-			project_key = split[project_index + 1]
-			# finds "repos" and add 1 to list position, grabbing the repo slug
-			repo_index = split.index('repos')
-			repo_slug = split[repo_index + 1]
-			parsed = [project_key, repo_slug]
-			parsed_list.append(parsed)
+			cleaned_1 = line.strip()
+			# removes space character if present. Example "project-key, repo-slug" to "project-key,repo-slug"
+			cleaned_2 = cleaned_1.replace(" ","")
+			# converts "projectkey-,repo-slug" to ["project", "repo"] 
+			split = cleaned_2.split(',')
+			parsed_list.append(split)
 	print("Parsed input list to:\n")
 	print(parsed_list)
 	print("\n\n")
@@ -90,42 +74,39 @@ def get_creds():
 	Vars.api_session.auth = (Vars.admin_username, Vars.admin_password)
 	return Vars
 
-def switch_inherited_access_to_individual(Vars):
-	# when a project is public, revoke the public setting but set all repos to public to match same results
-	# later we'll revoke public from the individual repos as necessary
-	for project in get_projects(Vars):
-		if Vars.project_public == True: # Set all sub repos as public, excluding the intended private repos
-			payload = {'public': 'False'}
-			endpoint_url = Vars.source_url + '/rest/api/1.0/projects/' + Vars.project_key
-			print("Setting project '"+Vars.project_key+"'' as private.")
-			r = ServerConnections.put_api(Vars, endpoint_url, payload) # revoke project public access
-			print("Status: " + str(r.status_code))
-			for repo in get_repos(Vars):
-				payload = {"public": "True"} # set public access on each repo, mimicing the original access patterns
-				endpoint_url = Vars.source_url + '/rest/api/1.0/projects/' + Vars.project_key + "/repos/" + Vars.repo_slug
-				print("Setting repo '"+Vars.repo_slug+"'' within project '"+Vars.project_key+"' as public to mimic original settings.")
-				r = ServerConnections.put_api(Vars, endpoint_url, payload)
-				print("Status: " + str(r.status_code))
-
-def revoke_public_access(Vars):
-	# parse list of repos that need to have public access revoked
-	list_of_repos = parse_input()
-	# example:
-	#list_of_repos = [["main", "primary"], ["fec", "repo1"], ["fec", "repo2"]] 
-	for repo in list_of_repos:
-		project_key = repo[0]
-		repo_slug = repo[1]
-		headers = {'X-Atlassian-Token': 'no-check', "Content-type": "application/json"}
-		payload = {"public": "False"}
-		endpoint_url = Vars.source_url + "/rest/api/1.0/projects/" + project_key + "/repos/" + repo_slug
-		print("Revoking public access to project '"+project_key+"', repo '"+repo_slug+"'")
+def switch_inherited_access_to_individual(input_line, Vars):
+	# Marking this specific project as private and all sub-repos as public to mirror original config
+	Vars.project_key = input_line[0]
+	payload = {'public': 'False'}
+	endpoint_url = Vars.source_url + '/rest/api/1.0/projects/' + Vars.project_key
+	print("Setting project '"+Vars.project_key+"'' as private.")
+	r = ServerConnections.put_api(Vars, endpoint_url, payload) # revoke project public access
+	print("Status: " + str(r.status_code))
+	for repo in get_repos(Vars):
+		payload = {"public": "True"} # set public access on each repo, mimicing the original access patterns
+		endpoint_url = Vars.source_url + '/rest/api/1.0/projects/' + Vars.project_key + "/repos/" + Vars.repo_slug
+		print("Setting repo '"+Vars.repo_slug+"'' within project '"+Vars.project_key+"' as public to mimic original settings.")
 		r = ServerConnections.put_api(Vars, endpoint_url, payload)
-		print("Status: "+str(r.status_code))
+		print("Status: " + str(r.status_code))
+
+def revoke_public_access(input_line, Vars):
+	project_key = input_line[0]
+	repo_slug = input_line[1]
+	headers = {'X-Atlassian-Token': 'no-check', "Content-type": "application/json"}
+	payload = {"public": "False"}
+	endpoint_url = Vars.source_url + "/rest/api/1.0/projects/" + project_key + "/repos/" + repo_slug
+	print("Revoking public access to project '"+project_key+"', repo '"+repo_slug+"'")
+	r = ServerConnections.put_api(Vars, endpoint_url, payload)
+	print("Status: "+str(r.status_code))
 
 def main():
 	get_creds()
-	switch_inherited_access_to_individual(Vars)
-	revoke_public_access(Vars)
+	input_list = parse_input()
+	for input_line in input_list:
+		if test_project(input_line, Vars) == True:  # True if public, False if private
+			switch_inherited_access_to_individual(input_line, Vars)
+		# after marking the project as public, if necessary, and setting the repos to public, mark the input as private
+		revoke_public_access(input_line, Vars)
 
 if __name__ == "__main__":
 	main()
